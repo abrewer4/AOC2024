ac778e82c01b2830ce0578b9b4dfd92eb66530f8
Alex Brewer <brewer.alex16@gmail.com>
2024-12-02 22:31:52 +1100

    Updated template file - also will no longer use utils.py

--- template.py
+++ template.py
@@ -1,6 +1,41 @@
-from utils import *
 
-lines = getLines("file.txt")
+def getInputLines(file):
+    lines = []
+    with open("file.txt") as input:
+        for line in input:
+            lines.append(line.strip())
+
+    return lines
 
 
-print(f"First line of file is: \n{lines[0]}")
+def printSample(input, lines):
+    print(f"First {lines} lines of input:")
+    for i in range(min(len(input), lines)):
+        print(input[i])
+
+
+def partOne():
+    answer = "???"
+
+    print("")
+    print(f"Part 1 answer is {answer}")
+
+
+def partTwo():
+    answer = "???"
+
+    print("")
+    print(f"Part 2 answer is {answer}")
+
+
+def main():
+    inputLines = getInputLines("1_1.txt")
+
+    printSample(inputLines, 3)
+
+    partOne()
+    # partTwo()
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -4,6 +4,6 @@ def getLines(file):
 
     with open(file) as input:
         for line in input:
-            linesArray.append(line[0:-1])
+            linesArray.append(line.strip())
 
     return linesArray
